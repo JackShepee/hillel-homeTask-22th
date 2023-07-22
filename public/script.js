21958e63386cefc5741dd4afafd7f1b722d58233
VSalamakha <salamakhaviktor@gmail.com>
2023-07-22 18:31:18 +0300

    initial commit

---
+++ public/script.js
@@ -0,0 +1,60 @@
+async function fetchQuestions() {
+  const response = await fetch("/questions");
+  const data = await response.json();
+
+  function checkAllAnswersSelected() {
+    const answerGroups = document.querySelectorAll(".answer-group");
+    let allAnswered = true;
+
+    answerGroups.forEach((group) => {
+      const selected = group.querySelector("input:checked");
+      if (!selected) {
+        allAnswered = false;
+      }
+    });
+
+    if (allAnswered) {
+      submitBtn.removeAttribute("disabled");
+    } else {
+      submitBtn.setAttribute("disabled", "true");
+    }
+  }
+
+  const questionsDiv = document.getElementById("questions");
+  data.forEach((question, index) => {
+    const questionElement = document.createElement("div");
+    questionElement.innerHTML = `
+    <p>${question.caption}</p>
+    <div class="answer-group">
+        Так: <input type="radio" name="question${index}" value="true">
+        Ні: <input type="radio" name="question${index}" value="false">
+    </div>`;
+    questionsDiv.appendChild(questionElement);
+  });
+
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener("change", checkAllAnswersSelected);
+  });
+}
+
+async function submitAnswers() {
+  const answers = [];
+  for (let i = 0; i < 4; i++) {
+    const value = document.querySelector(
+      `input[name="question${i}"]:checked`
+    ).value;
+    answers.push(value === "true");
+  }
+
+  const response = await fetch("/answers", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(answers),
+  });
+
+  const result = await response.json();
+  alert(`Ви відповіли правильно на ${result.correctCount} питань`);
+}
+
+fetchQuestions();
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const questions = [
+  {
+    caption: "Подія натискання на елемент називається click?",
+    correctAnswer: true,
+  },
+  {
+    caption: "Усередині розмітки не можна додати обробник події?",
+    correctAnswer: false,
+  },
+  {
+    caption:
+      "Припинити спливання події можна за допомогою метода stopImmediatePropagation?",
+    correctAnswer: false,
+  },
+  {
+    caption:
+      "Припинити спливання події можна за допомогою метода stopPropagation?",
+    correctAnswer: true,
+  },
+];
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/questions", (req, res) => {
+  res.json(questions);
+});
+
+app.post("/answers", (req, res) => {
+  const userAnswers = req.body;
+  let correctCount = 0;
+
+  questions.forEach((question, index) => {
+    if (question.correctAnswer === userAnswers[index]) {
+      correctCount++;
+    }
+  });
+
+  res.json({ correctCount });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
